8617603ab6d36f6ef19de3afa2140f2104d6a8b6
v-iacovella <vittorio.iacovella@gmail.com>
2013-06-25 11:52:51 +0300

    Update installSoftware.sh

--- installSoftware.sh
+++ installSoftware.sh
@@ -1,121 +1,115 @@
 #!/bin/bash
 
 #MYLOCALDIR=$1
-MYLOCALDIR=/nilab0/iacovella/myLocal
-LAPACKLIB=/nilab0/iacovella/pkgSrc/tarballs/lapack-3.4.2.tgz
+
+MYLOCALDIR=~/myLocal
+PKGDIR=~/pkgSrc
+LAPACKLIB=~/pkgSrc/tarballs/lapack-3.4.2.tgz
+
 export LD_LIBRARY_PATH=$MYLOCALDIR/lib
 export PATH=$MYLOCALDIR/bin:$PATH
 echo $MYLOCALDIR $LD_LIBRARY_PATH
 
 # Unpack the directory
-#tar xvf pkgSrc.tar.gz
+tar xvf pkgSrc.tar.gz
 
 cd pkgSrc/tarballs
 
 # Build Bzip2
-#tar xvf bzip2-1.0.6.tar.gz
-#cd bzip2-1.0.6
-#make -f Makefile-libbz2_so
-#make
-#make install PREFIX=$MYLOCALDIR
-#cd ../
+tar xvf bzip2-1.0.6.tar.gz
+cd bzip2-1.0.6
+make -f Makefile-libbz2_so
+make
+make install PREFIX=$MYLOCALDIR
+cd ../
 
 # Build sqlite3
-#tar xvf sqlite-autoconf-3071700.tar.gz
-#cd sqlite-autoconf-3071700
-#./configure --prefix=$MYLOCALDIR
-#make
-#make install
-#cd ../
+tar xvf sqlite-autoconf-3071700.tar.gz
+cd sqlite-autoconf-3071700
+./configure --prefix=$MYLOCALDIR
+make
+make install
+cd ../
 
 # Build ncurses
-#tar xvf ncurses-5.9.tar.gz
-#cd ncurses-5.9
-#./configure --with-shared --prefix $MYLOCALDIR
-#make
-#make install
-#cd ../
+tar xvf ncurses-5.9.tar.gz
+cd ncurses-5.9
+./configure --with-shared --prefix $MYLOCALDIR
+make
+make install
+cd ../
 
 # Build readlines
-#tar xvf readline-master.tar.gz
-#cd readline-master
-#./configure --with-shared --prefix $MYLOCALDIR
-#make
-#make install
-#cd ../
+tar xvf readline-master.tar.gz
+cd readline-master
+./configure --with-shared --prefix $MYLOCALDIR
+make
+make install
+cd ../
 
 # Build openssl
-#tar xvf openssl-1.0.1e
-#cd openssl-1.0.1e
-#./config --prefix=$MYLOCALDIR shared
-#make
-#make install
-#cd ../
+tar xvf openssl-1.0.1e
+cd openssl-1.0.1e
+./config --prefix=$MYLOCALDIR shared
+make
+make install
+cd ../
 
 # Build Python
-#tar xvf Python-2.7.5.tgz
-#cd Python-2.7.5
-#./configure --prefix=$MYLOCALDIR --enable-shared
-#make
-#make install
-#cd ../
+tar xvf Python-2.7.5.tgz
+cd Python-2.7.5
+./configure --prefix=$MYLOCALDIR --enable-shared
+make
+make install
+cd ../
 
 # Building algebra packages:
-#cd ../../
-#cd $MYLOCALDIR
-#mkdir opt
-#mkdir opt/ATLASBuild\
+cd ../../
+cd $MYLOCALDIR
+mkdir opt
+mkdir opt/ATLASBuild
 
-#cd opt/ATLASBuild
-#/nilab0/iacovella/pkgSrc/tarballs/ATLAS/configure --prefix=$MYLOCALDIR -Fa alg -fPIC --with-netlib-lapack-tarfile=$LAPACKLIB
+cd opt/ATLASBuild
+~/pkgSrc/tarballs/ATLAS/configure --prefix=$MYLOCALDIR -Fa alg -fPIC --with-netlib-lapack-tarfile=$LAPACKLIB
 #make
 #make install
 #cd ../../../
 
 # From now on: Python libraries
+
 # Setuptools
-#cd pkgSrc/tarballs
-#tar xvf setuptools-0.7.4.tar.gz
-#cd setuptools-0.7.4
-
-#python setup.py install --prefix=$MYLOCALDIR
-#cd ../
-
-# Nose
-#tar xvf nose*
-#cd nose*
-
-#python setup.py install --prefix=$MYLOCALDIR
-
-#cd ..
-
-# Cython
-#tar xvf Cython*
-#cd Cython*
-
-#python setupegg.py install --prefix=$MYLOCALDIR
+cd pkgSrc/tarballs
+tar xvf setuptools*
+cd setuptools*
+python setup.py install --prefix=$MYLOCALDIR
 cd ../
 
+# Nose
+tar xvf nose*
+cd nose*
+python setup.py install --prefix=$MYLOCALDIR
+cd ..
+
+# Cython
+tar xvf Cython*
+cd Cython*
+python setupegg.py install --prefix=$MYLOCALDIR
+cd ../../
+
 # Numpy
-#cd numpy
-
-#python setupegg.py install --prefix=$MYLOCALDIR
-
-#cd ..
+cd numpy
+python setupegg.py install --prefix=$MYLOCALDIR
+cd ..
 
 # Scipy
-#cd scipy
-
-#python setupegg.py install --prefix=$MYLOCALDIR
-
-#cd ..
+cd scipy
+python setupegg.py install --prefix=$MYLOCALDIR
+cd ..
 
 # Networkx
-#cd networkx 
-
-#python setup_egg.py install --prefix=$MYLOCALDIR
-
-#cd ..
+cd networkx 
+python setup_egg.py install --prefix=$MYLOCALDIR
+cd ..
 
 # traits
 cd traits
@@ -124,16 +118,12 @@ cd ..
 
 # Nibabel
 cd nibabel
-
 python setup_egg.py install --prefix=$MYLOCALDIR
-
 cd ..
 
 # Nipype
 cd nipype
-
 python setup_egg.py install --prefix=$MYLOCALDIR
-
 cd ..
 
 # END
